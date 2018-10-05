448a086bd3aa5b1a1fc5463f8a685c110e98c9da
Eric Harris <ericmharris@gmail.com>
2018-10-05 12:21:42 -0700

    unpin reqs

--- setup.py
+++ setup.py
@@ -30,11 +30,11 @@ with open_relative("README.rst") as f:
 
 requires = [
     "certifi",
-    "configparser>=3.5.0b1",
-    "cryptography>=2.1.3",
-    "pyOpenSSL<=17.4.0",
-    "python-dateutil>=2.5.3,<=2.7.3",
-    "pytz>=2016.10",
+    "configparser",
+    "cryptography",
+    "pyOpenSSL",
+    "python-dateutil",
+    "pytz",
 ]
 
 setup(
